ff7e691ad876bb3de0e6f30480f31cb95c0a1a0a
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-12-05 16:02:13 +0100

    fix: add collection name in test reload keep state

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -34,16 +34,17 @@ def test_reload_keep_state(docker_compose):
     docs = DocumentArray([Document(embedding=np.random.rand(3)) for _ in range(2)])
     f = Flow().add(
         uses=MilvusIndexer,
-        uses_with={'n_dim': 3},
+        uses_with={'collection_name': 'test_keep_state', 'n_dim': 3},
     )
 
     with f:
         f.index(docs)
         first_search = f.search(inputs=docs)
+        assert len(first_search[0].matches) == 2
 
     with f:
         second_search = f.search(inputs=docs)
-        assert len(first_search[0].matches) == len(second_search[0].matches)
+        assert len(second_search[0].matches) == 2
 
 
 def test_filtering(docker_compose):
