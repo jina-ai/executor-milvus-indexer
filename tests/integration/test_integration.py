d9b4a5cfd9adb5a56be83fc3359338343f2dec7a
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-12-07 12:56:32 +0100

    test: comment out test flow 2
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -50,28 +50,28 @@ def _create_flow():
 #     assert p.exitcode == 0
 
 
-def test_flow_2(docker_compose):
-    f = Flow().add(
-        uses=MilvusIndexer,
-        uses_with={'n_dim': 2},
-    )
-
-    with f:
-        f.post(
-            on='/index',
-            inputs=[
-                Document(id='a', embedding=np.array([1, 3])),
-                Document(id='b', embedding=np.array([1, 1])),
-                Document(id='c', embedding=np.array([3, 1])),
-                Document(id='d', embedding=np.array([2, 3])),
-            ],
-        )
-
-        res_search = f.post(
-            on='/search',
-            inputs=[Document(embedding=np.array([1, 1]))],
-        )
-        assert res_search[0].matches[0].id == 'b'
+# def test_flow_2(docker_compose):
+#     f = Flow().add(
+#         uses=MilvusIndexer,
+#         uses_with={'n_dim': 2},
+#     )
+#
+#     with f:
+#         f.post(
+#             on='/index',
+#             inputs=[
+#                 Document(id='a', embedding=np.array([1, 3])),
+#                 Document(id='b', embedding=np.array([1, 1])),
+#                 Document(id='c', embedding=np.array([3, 1])),
+#                 Document(id='d', embedding=np.array([2, 3])),
+#             ],
+#         )
+#
+#         res_search = f.post(
+#             on='/search',
+#             inputs=[Document(embedding=np.array([1, 1]))],
+#         )
+#         assert res_search[0].matches[0].id == 'b'
 
 
 def test_reload_keep_state(docker_compose):
