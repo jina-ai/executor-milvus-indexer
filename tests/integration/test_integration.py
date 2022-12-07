a8664430ecc0306f1b73494e9fcdbb74d4c59fa1
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-12-07 12:51:01 +0100

    test: remove test with multiple processes from test integration
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -41,13 +41,37 @@ def _create_flow():
 
         assert q.exitcode == 0
 
+#
+# def test_flow(docker_compose):
+#     p = multiprocessing.Process(target=_create_flow)
+#     p.start()
+#     p.join()
+#
+#     assert p.exitcode == 0
 
-def test_flow(docker_compose):
-    p = multiprocessing.Process(target=_create_flow)
-    p.start()
-    p.join()
 
-    assert p.exitcode == 0
+def test_flow_2(docker_compose):
+    f = Flow().add(
+        uses=MilvusIndexer,
+        uses_with={'n_dim': 2},
+    )
+
+    with f:
+        f.post(
+            on='/index',
+            inputs=[
+                Document(id='a', embedding=np.array([1, 3])),
+                Document(id='b', embedding=np.array([1, 1])),
+                Document(id='c', embedding=np.array([3, 1])),
+                Document(id='d', embedding=np.array([2, 3])),
+            ],
+        )
+
+        res_search = f.post(
+            on='/search',
+            inputs=[Document(embedding=np.array([1, 1]))],
+        )
+        assert res_search[0].matches[0].id == 'b'
 
 
 def test_reload_keep_state(docker_compose):
