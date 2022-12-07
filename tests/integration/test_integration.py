86d5ab56acee46ebc156ac98f3d807805c7c8bea
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-12-07 12:44:56 +0100

    test: remove explicit os env var from test integration
    Signed-off-by: anna-charlotte <charlotte.gerhaher@jina.ai>

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,13 +1,10 @@
 import multiprocessing
-import pytest
 import numpy as np
 
 from docarray import Document, DocumentArray
 from jina import Client, Flow
 
 from executor import MilvusIndexer
-import os
-os.environ['GRPC_ENABLE_FORK_SUPPORT'] = 'false'
 
 
 def _send_request(gateway_port, protocol):
@@ -46,7 +43,6 @@ def _create_flow():
 
 
 def test_flow(docker_compose):
-    os.environ['GRPC_ENABLE_FORK_SUPPORT'] = 'false'
     p = multiprocessing.Process(target=_create_flow)
     p.start()
     p.join()
